38222b6b1eba220184ddf1c9ddbb6557c778f6b4
HansNilsen <69566153+HansNilsen@users.noreply.github.com>
2020-08-14 15:42:38 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,154 @@
+// DND morality questions
+
+//TWO: Upon trying to enter a village, you are stopped by a sleazy-looking guard at the gate.  He demands you pay a toll before entering.  Which option describes what you would most likely do?
+
+// 1) I'd pay the toll.
+// 2) I'd argue with the guard, but if he won't budge, I'll end up paying.
+// 3) I'd try and bribe the guard or use a charisma check to convince him to waive the toll for an absolute stud like me.
+// 4) I'd threaten him, then attack him if he refuses to let me through.
+// 5) I'd rally the nearby townsfolk and peasants into a mob, then lead them in a march on the city gates and let the ensuing anarchy clear a path for me.
+
+//THREE: You are returning from an adventure on a relatively safe road, with one expensive and powerful health potion left in your inventory.  You encounter a gravely wounded man who begs you for help.  Which option best describes what you woul do?
+// 1) I'd give him my health potion, without question.
+// 2) I wouldn't want to give up my health potion, but I'd offer to escort him to the nearest village or give him some of my less valuable supplies.
+// 3) I'd shrug, say I wish there was more I could do, then continue on my way.
+// 4) I'd make fun of him, then offer him the potion, but only if he's able to offer some handsome compensation in return.
+// 5) I'd mug him and steal anything of value on him, then tell him how ugly he is.
+
+//FOUR: After being offered a handsome reward to do so by the local village elders, you are hunting the captain of a clan of bandits.  Upon his capture, he gives you a counteroffer: he'll triple the amount of gold the village promised if you free him.  Additionally, if you help him sack the village, he'll give you quadruple the amount.  What would you do?
+
+// 1) Never!  I'm bringing this scum to justice.
+// 2) I'd accept the offer to free him, but only if he promises to renounce the bandit's life and never hurt anyone again.
+// 3) I'd free him, but I wouldn't help him sack anyone.
+// 4) I'd take both offers.
+// 5) I'd accept the deal, kill him, then usurp control over his bandit clan and attempt to conquer the whole province.
+
+//FIVE: Would you ever cheat in an academic setting?
+// 1) Never.
+// 2) I'm against it, but if I'm really stressed out, the outcome is significant enough, and I'm positive I can get away with it, I'd probably give into the temptation.
+// 3) Only on trivial things, if I'm stressed out and just need to get an assignment out of the way.
+// 4) Only occasionally to bump up my grade and ensure I don't fail, but I'd still make sure I do most of the work and learn the material.
+// 5) I'd gladly cheat on anything without a second thought.
+
+//SIX: Inside a department store operated by a prosperous nationwide corporate chain you notice someone successfully shoplift an item of value from the store and leave without suspicion.  Thoughts?
+// 1) Scumbag.
+// 2) Slight disapproval.
+// 3) I don't care.
+// 4) Amusement.
+// 5) Admiration.
+
+//SEVEN: You are given no information about a person EXCEPT that they have served prison time for a crime they committed.  What is your first impression?
+// 1) That's not good - I hope they've reformed, but I'd have very little respect for them.
+// 2) It would make me wary, but I'd keep an open mind.
+// 3) I don't care.
+// 4) I'd be curious and want to get to know them better - they sound exciting.
+// 5) They're most likely justified or wrongly convicted - my opinion of them would start off high.
+
+//EIGHT: When you hear the word "revolution" what word best describes your immediate reaction?
+// 1) Visceral disgust
+// 2) I roll my eyes
+// 3) Nothing, I'll hear them out
+// 4) No idea what they want, but I'm probably down
+// 5) It was probably me who said it
+
+//NINE: Which faction of a fictional universe would you be most likely to join?
+// 1) The noble town guard of a well-respected city
+// 2) Rebels fighting for freedom from an oppressive dictator
+// 3) Keep me out of it, I'd mind my own business
+// 4) The secret police of an autocratic government
+// 5) A clan of violent, greedy, marauding bandits
+
+// 1) The Galactic Empire is my ideal political system.
+// 2) There is never an excuse to commit crime, no matter what the reason.
+// 3) Breaking the law is never acceptable except in extreme circumstances, such as in the face of tyranny, severe oppression, or immediate physical harm to innocent people.
+// 4) Civil disobedience - breaking the law - should be encouraged if it aims to challenge an unjust law, even if that law causes no *physical* harm to anyone.
+
+//ONE: Choose the statement with which you agree the most.
+// 1) Breaking the law is never acceptable.
+// 2) Breaking the law is only acceptable in extreme circumstances.
+// 3) Breaking the law is occasionally acceptable.
+// 4) Breaking the law is generally acceptable.
+// 5) What the law is bears no significance in my decision making.
+
+const questions = [
+  {
+    "question": "Choose the statement with which you agree the most.",
+    "answer1:": "Breaking the law is never acceptable.",
+    "answer1Total": "1",
+  },
+  {
+
+  }
+]
+
+
+
+const questions = [
+  {
+    "question": "You encounter a man being robbed by a group of thugs.  What do you do?",
+    "answer1": "Kick their ass.",
+    "answer1Total": "1",
+    "answer2": "Try to reason with them to leave the man alone and return his belongings.",
+    "answer2Total": "2",
+    "answer3": "Use the Force to cause a distraction and help the man flee, causing as little harm as possible.",
+    "answer3Total": "3"
+  },
+  {
+    "question": "I am very imaginative.",
+    "answer1": "Agree",
+    "answer1Total": "1",
+    "answer2": "Neutral",
+    "answer2Total": "2",
+    "answer3": "Disagree",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Select in which order you would value these \"Money, Love & Career",
+    "answer1": "Love, Career, Money",
+    "answer1Total": "1",
+    "answer2": "Money, Career, Love",
+    "answer2Total": "3",
+    "answer3": "Career, Love, Money",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Best Sentence to describe you?",
+    "answer1": "You feel superior to other people.",
+    "answer1Total": "3",
+    "answer2": "You consider yourself more practical than creative.",
+    "answer2Total": "2",
+    "answer3":
+      "Winning a debate matters less to you than making sure no one gets upset.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which best describes your relationship with food",
+    "answer1": "You tend to over-eat when you have company.",
+    "answer1Total": "1",
+    "answer2": "You tend to eat snacks secretly.",
+    "answer2Total": "2",
+    "answer3": "You prepare food and don\’t even look at the recipe.",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "You make plans with a friend and they cancel on you, what do you do?",
+    "answer1":
+      "Say \"whatever\" and plan a night that'll be GREAT so they don't cancel again.",
+    "answer1Total": "3",
+    "answer2": "Feel hurt because you were looking forward to tonight.",
+    "answer2Total": "2",
+    "answer3": "No problem, you kinda wanted to stay home anyway.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which of the following colours do you like most?",
+    "answer1": "Black",
+    "answer1Total": "1",
+    "answer2": "Yellow or light blue",
+    "answer2Total": "2",
+    "answer3": "Red or orange",
+    "answer3Total": "3"
+  }
+]
