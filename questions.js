5fa6b7f3f778896d3e9939f19764f871aa51c68b
HansNilsen <69566153+HansNilsen@users.noreply.github.com>
2020-08-15 14:25:18 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -74,23 +74,23 @@ const questions = [
     "answer2Total": "2",
     "answer3": "Breaking the law is occasionally acceptable.",
     "answer3Total": "3"
-    //"answer4": "Breaking the law is generally acceptable.",
-    //"answer4Total": "4",
-    //"answer5": "What the law is bears no significance in my decision making.",
-    //"answer5Total": "5"
+    "answer4": "Breaking the law is generally acceptable.",
+    "answer4Total": "4",
+    "answer5": "What the law is bears no significance in my decision making.",
+    "answer5Total": "5"
   },
   {
-    "question": "Upon trying to enter a village, you are stopped by a sleazy-looking guard at the gate.  He demands you pay a toll before entering.  Which option describes what you would most likely do?",
+    "question": "You are stopped from entering a city by a sleazy-looking guard at the gate.  He demands you pay a toll before entering.  Which option describes what you would most likely do?",
     "answer1": "I\'d pay the toll.",
     "answer1Total": "1",
     "answer2": "I\'d argue with the guard, but if he won\'t budge, I\'ll end up paying.",
     "answer2Total": "2",
     "answer3": "I\'d try and bribe the guard or use a charisma check to convince him to waive the toll for an absolute stud like me.",
     "answer3Total": "3"
-    //"answer4": "I'd threaten him, then attack him if he refuses to let me through.",
-    //"answer4Total": "4",
-    //"answer5": "I'd rally the nearby townsfolk and peasants into a mob, then lead them in a march on the city gates and let the ensuing anarchy clear a path for me.",
-    //"answer5Total": "5"
+    "answer4": "I\'d threaten him, then attack him if he refuses to let me through.",
+    "answer4Total": "4",
+    "answer5": "I\'d rally the nearby townsfolk and peasants into a mob, then lead them in a march on the city gates and let the ensuing anarchy clear a path for me.",
+    "answer5Total": "5"
   }
 ]
 
