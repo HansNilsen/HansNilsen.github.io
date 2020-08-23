51527dab2ffbacfd7515f7bf37bcd0b34d167432
HansNilsen <69566153+HansNilsen@users.noreply.github.com>
2020-08-22 23:30:21 -0700

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,101 +1,170 @@
-/*
-// DND morality questions
-
-
-// 1) 
-// 2) 
-// 3) I'd free him, but I wouldn't help him sack anyone.
-// 4) I'd take both offers.
-// 5) I'd accept the deal, kill him, then usurp control over his bandit clan and attempt to conquer the whole province.
-
-//FIVE: Would you ever cheat in an academic setting?
-// 1) Never.
-// 2) I'm against it, but if I'm really stressed out, the outcome is significant enough, and I'm positive I can get away with it, I'd probably give into the temptation.
-// 3) Only on trivial things, if I'm stressed out and just need to get an assignment out of the way.
-// 4) Only occasionally to bump up my grade and ensure I don't fail, but I'd still make sure I do most of the work and learn the material.
-// 5) I'd gladly cheat on anything without a second thought.
-
-//SIX: Inside a department store operated by a prosperous nationwide corporate chain you notice someone successfully shoplift an item of value from the store and leave without suspicion.  Thoughts?
-// 1) Scumbag.
-// 2) Slight disapproval.
-// 3) I don't care.
-// 4) Amusement.
-// 5) Admiration.
-
-//SEVEN: You are given no information about a person EXCEPT that they have served prison time for a crime they committed.  What is your first impression?
-// 1) That's not good - I hope they've reformed, but I'd have very little respect for them.
-// 2) It would make me wary, but I'd keep an open mind.
-// 3) I don't care.
-// 4) I'd be curious and want to get to know them better - they sound exciting.
-// 5) They're most likely justified or wrongly convicted - my opinion of them would start off high.
-
-//EIGHT: When you hear the word "revolution" what word best describes your immediate reaction?
-// 1) Visceral disgust
-// 2) I roll my eyes
-// 3) Nothing, I'll hear them out
-// 4) No idea what they want, but I'm probably down
-// 5) It was probably me who said it
-
-//NINE: Which faction of a fictional universe would you be most likely to join?
-// 1) The noble town guard of a well-respected city
-// 2) Rebels fighting for freedom from an oppressive dictator
-// 3) Keep me out of it, I'd mind my own business
-// 4) The secret police of an autocratic government
-// 5) A clan of violent, greedy, marauding bandits
-*/
-
 const questions = [
-  {
-    "question": "Choose the statement with which you agree the most.",
-    "answer1": "Breaking the law is never acceptable.",
-    "answer1Total": "1",
-    "answer2": "Breaking the law is only acceptable in extreme circumstances.",
-    "answer2Total": "2",
-    "answer3": "Breaking the law is occasionally acceptable.",
-    "answer3Total": "3",
-    "answer4": "Breaking the law is generally acceptable.",
-    "answer4Total": "4",
-    "answer5": "What the law is bears no significance in my decision making.",
-    "answer5Total": "5"
-  },
-  {
-    "question": "You are stopped from entering a city by a sleazy-looking guard at the gate.  He demands you pay a toll before entering.  Which option describes what you would most likely do?",
-    "answer1": "I\'d pay the toll.",
-    "answer1Total": "1",
-    "answer2": "I\'d argue with the guard, but if he won\'t budge, I\'ll end up paying.",
-    "answer2Total": "2",
-    "answer3": "I\'d try and bribe the guard or use a charisma check to convince him to waive the toll for an absolute stud like me.",
-    "answer3Total": "3",
-    "answer4": "I\'d threaten him, then attack him if he refuses to let me through.",
-    "answer4Total": "4",
-    "answer5": "I\'d rally the nearby townsfolk and peasants into a mob, then lead them in a march on the city gates and let the ensuing anarchy clear a path for me.",
-    "answer5Total": "5"
-  },
-  {
-    "question": "You are returning from an adventure on a relatively safe road, with one expensive and powerful health potion left in your inventory.  You encounter a gravely wounded man who begs you for help.  Which option best describes what you woul do?",
-    "answer1": "I\'d give him my health potion, without question.",
-    "answer1Total": "1",
-    "answer2": "I wouldn\'t want to give up my health potion, but I\'d offer to escort him to the nearest village or give him some of my less valuable supplies.",
-    "answer2Total": "2",
-    "answer3": "I\'d shrug, say I wish there was more I could do, then continue on my way.",
-    "answer3Total": "3",
-    "answer4": "I\'d make fun of him, then offer him the potion, but only if he\'s able to offer some handsome compensation in return.",
-    "answer4Total": "4",
-    "answer5": "I\'d mug him and steal anything of value on him, then tell him how ugly he is.",
-    "answer5Total": "5"
-  },
-  {
-    "question": "After being offered a handsome reward to do so by the local village elders, you are hunting the captain of a clan of bandits.  Upon his capture, he gives you a counteroffer: he'll triple the amount of gold the village promised if you free him.  Additionally, if you help him sack the village, he'll give you quadruple the amount.  What would you do?",
-    "answer1": "Never!  I\'m bringing this scum to justice.",
-    "answer1Total": "1",
-    "answer2": "I\'d accept the offer to free him, but only if he promises to renounce the bandit\'s life and never hurt anyone again.",
-    "answer2Total": "2",
-    "answer3": "",
-    "answer3Total": "3",
-    "answer4": "",
-    "answer4Total": "4",
-    "answer5": "",
-    "answer5Total": "5"
-  }
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "What the law is bears no significance in my decision making.",
+        "question": "Choose the statement with which you agree the most.",
+        "answer1": "Breaking the law is never acceptable.",
+        "answer3": "Breaking the law is occasionally acceptable.",
+        "answer2": "Breaking the law is only acceptable in extreme circumstances.",
+        "answer4": "Breaking the law is generally acceptable.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "I'd rally the nearby townsfolk and peasants into a mob, then lead them in a march on the city gates and let the ensuing anarchy clear a path for me.",
+        "question": "You are stopped from entering a city by a sleazy-looking guard at the gate.  He demands you pay a toll before entering.  Which option describes what you would most likely do?",
+        "answer1": "I'd pay the toll.",
+        "answer3": "I'd try and bribe the guard or use a charisma check to convince him to waive the toll for an absolute stud like me.",
+        "answer2": "I'd argue with the guard, but if he won't budge, I'll end up paying.",
+        "answer4": "I'd threaten him, then attack him if he refuses to let me through.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "I'd mug him and steal anything of value on him, then tell him how ugly he is.",
+        "question": "You are returning from an adventure on a relatively safe road, with one expensive and powerful health potion left in your inventory.  You encounter a gravely wounded man who begs you for help.  Which option best describes what you woul do?",
+        "answer1": "I'd give him my health potion, without question.",
+        "answer3": "I'd shrug, say I wish there was more I could do, then continue on my way.",
+        "answer2": "I wouldn't want to give up my health potion, but I'd offer to escort him to the nearest village or give him some of my less valuable supplies.",
+        "answer4": "I'd make fun of him, then offer him the potion, but only if he's able to offer some handsome compensation in return.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Moral"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "I'd accept the deal, kill him, then usurp control over his bandit clan and attempt to conquer the whole province.",
+        "question": "After being offered a handsome reward to do so by the local village elders, you are hunting the captain of a clan of bandits.  Upon his capture, he gives you a counteroffer: he'll triple the amount of gold the village promised if you free him.  Additionally, if you help him sack the village, he'll give you quadruple the amount.  What would you do?",
+        "answer1": "Never!  I'm bringing this scum to justice.",
+        "answer3": "I'd free him, but I wouldn't help him sack anyone.",
+        "answer2": "I'd accept the offer to free him, but only if he promises to renounce the bandit's life and never hurt anyone again.",
+        "answer4": "I'd take both offers.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Moral"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "I'd gladly cheat on anything without a second thought.",
+        "question": "Would you ever cheat in an academic setting?",
+        "answer1": "Never.",
+        "answer3": "Only on trivial things, if I'm stressed out and just need to get an assignment out of the way.",
+        "answer2": "I'm against it, but if I'm really stressed out, the outcome is significant enough, and I'm positive I can get away with it, I'd probably give into the temptation.",
+        "answer4": "Only occasionally to bump up my grade and ensure I don't fail, but I'd still make sure I do most of the work and learn the material.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "Admiration.",
+        "question": "Inside a department store operated by a prosperous nationwide corporate chain you notice someone successfully shoplift an item of value from the store and leave without suspicion.  Thoughts?",
+        "answer1": "Scumbag.",
+        "answer3": "I don't care.",
+        "answer2": "Slight disapproval.",
+        "answer4": "Amusement.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "They're most likely justified or wrongly convicted - my opinion of them would start off high.",
+        "question": "You are given no information about a person EXCEPT that they have served prison time for a crime they committed.  What is your first impression?",
+        "answer1": "That's not good - I hope they've reformed, but I'd have very little respect for them.",
+        "answer3": "I don't care.",
+        "answer2": "It would make me wary, but I'd keep an open mind.",
+        "answer4": "I'd be curious and want to get to know them better - they sound exciting.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "It was probably me who said it.",
+        "question": "When you hear the word \"revolution\" what word best describes your immediate reaction?",
+        "answer1": "Visceral disgust.",
+        "answer3": "Nothing, I'll hear them out.",
+        "answer2": "I roll my eyes.",
+        "answer4": "No idea what they want, but I'm probably down.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Law"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "A clan of violent, greedy, marauding bandits",
+        "question": "Which faction of a fictional universe would you be most likely to join?",
+        "answer1": "The noble town guard of a well-respected city",
+        "answer3": "Keep me out of it, I'd mind my own business.",
+        "answer2": "Rebels fighting for freedom from an oppressive dictator",
+        "answer4": "The secret police of an autocratic government",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Both"
+    },
+    {
+        "answer4Total": "",
+        "answer3Total": "",
+        "answer1Total": "",
+        "answer5": "Sauron is my role model.",
+        "question": "Which sentence best describes your moral philosophy?",
+        "answer1": "You behave righteously and do the right thing, in all circumstances.",
+        "answer3": "Fighting for good or evil is pointless; it's better to look out for your own interests and take care of yourself.",
+        "answer2": "You should try to do the right thing, but sometimes it's tricky.",
+        "answer4": "I'm willing to hurt people's livelihoods to achieve my devious goals.",
+        "answer2Total": "",
+        "answer5Total": "",
+        "qType": "Moral"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "No.  In fact, it fills me with Scrooge-like joy.",
+        "question": "You see a homeless man begging on the street.  Do you feel bad?",
+        "answer1": "Yes.",
+        "answer3": "Eh... not really?",
+        "answer2": "Kinda.",
+        "answer4": "No.  Natural selection exists for a reason.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Moral"
+    },
+    {
+        "answer4Total": "1",
+        "answer3Total": "0",
+        "answer1Total": "-2",
+        "answer5": "Stop at nothing to achieve a brutal, oppressive dictatorship.",
+        "question": "You have just been crowned Emperor.  What's your plan?",
+        "answer1": "Fight relentlessly for the well-being of my citizens and help the downtrodden, no matter the political difficulty.",
+        "answer3": "Stay the course, stay in power, and use the office to advance my own interests.",
+        "answer2": "Navigate the politics of the realm carefully and try to pass some good laws, but try not to ruffle feathers.",
+        "answer4": "Tighten my grip over the realm, oppress the marginalized, and squeeze out the kingdom's wealth to fuel my own singular power.",
+        "answer2Total": "-1",
+        "answer5Total": "2",
+        "qType": "Moral"
+    }
 ]
-
